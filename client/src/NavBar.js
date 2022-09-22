5b7cb42c65bd2dddedac1e3fea868b5794380e95
Kelan6 <101379206+Kelan6@users.noreply.github.com>
2022-09-22 10:42:10 -0600

    flowbite

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -1,15 +1,82 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+// import {Link} from 'react-router-dom'
+import { Navbar, Dropdown, Avatar } from 'flowbite-react'
 
 function NavBar() {
 
   return (
-    <div id='navbar'>
-                <Link className="route-link" to="/">Home</Link>
-                <Link className="route-link" to="/planner">Planner</Link>
-                <Link className="route-link" to="/signup">Signup</Link>
-                <Link className="route-link" to="/login">Login</Link>
-    </div>
+    // <div id='navbar'>
+    //             <Link className="route-link" to="/">Home</Link>
+    //             <Link className="route-link" to="/planner">Planner</Link>
+    //             <Link className="route-link" to="/signup">Signup</Link>
+    //             <Link className="route-link" to="/login">Login</Link>
+    // </div>
+
+<Navbar
+fluid={true}
+rounded={true}
+>
+<Navbar.Brand href="https://flowbite.com/">
+  <img
+    src="https://flowbite.com/docs/images/logo.svg"
+    className="mr-3 h-6 sm:h-9"
+    alt="Flowbite Logo"
+  />
+  <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+    Flowbite
+  </span>
+</Navbar.Brand>
+<div className="flex md:order-2">
+  <Dropdown
+    arrowIcon={false}
+    inline={true}
+    label={<Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded={true}/>}
+  >
+    <Dropdown.Header>
+      <span className="block text-sm">
+        Bonnie Green
+      </span>
+      <span className="block truncate text-sm font-medium">
+        name@flowbite.com
+      </span>
+    </Dropdown.Header>
+    <Dropdown.Item>
+      Dashboard
+    </Dropdown.Item>
+    <Dropdown.Item>
+      Settings
+    </Dropdown.Item>
+    <Dropdown.Item>
+      Earnings
+    </Dropdown.Item>
+    <Dropdown.Divider />
+    <Dropdown.Item>
+      Sign out
+    </Dropdown.Item>
+  </Dropdown>
+  <Navbar.Toggle />
+</div>
+<Navbar.Collapse>
+  <Navbar.Link
+    href="/navbars"
+    active={true}
+  >
+    Home
+  </Navbar.Link>
+  <Navbar.Link href="/navbars">
+    About
+  </Navbar.Link>
+  <Navbar.Link href="/navbars">
+    Services
+  </Navbar.Link>
+  <Navbar.Link href="/navbars">
+    Pricing
+  </Navbar.Link>
+  <Navbar.Link href="/navbars">
+    Contact
+  </Navbar.Link>
+</Navbar.Collapse>
+</Navbar>
   )
 }
 
