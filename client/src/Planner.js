442f72af5beb968ff84169d563ca11281b9f0822
Kelan6 <101379206+Kelan6@users.noreply.github.com>
2022-09-22 13:29:04 -0600

    aerf

--- client/src/Planner.js
+++ client/src/Planner.js
@@ -3,95 +3,70 @@ import {Table} from 'flowbite-react'
 
 function Planner() {
   return (
-    <Table>
-  <Table.Head>
-    <Table.HeadCell>
-      Product name
-    </Table.HeadCell>
-    <Table.HeadCell>
-      Color
-    </Table.HeadCell>
-    <Table.HeadCell>
-      Category
-    </Table.HeadCell>
-    <Table.HeadCell>
-      Price
-    </Table.HeadCell>
-    <Table.HeadCell>
-      <span className="sr-only">
-        Edit
-      </span>
-    </Table.HeadCell>
-  </Table.Head>
-  <Table.Body className="divide-y">
-    <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-      <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-        Apple MacBook Pro 17"
-      </Table.Cell>
-      <Table.Cell>
-        Sliver
-      </Table.Cell>
-      <Table.Cell>
-        Laptop
-      </Table.Cell>
-      <Table.Cell>
-        $2999
-      </Table.Cell>
-      <Table.Cell>
-        <a
-          href="/tables"
-          className="font-medium text-blue-600 hover:underline dark:text-blue-500"
-        >
-          Edit
-        </a>
-      </Table.Cell>
-    </Table.Row>
-    <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-      <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-        Microsoft Surface Pro
-      </Table.Cell>
-      <Table.Cell>
-        White
-      </Table.Cell>
-      <Table.Cell>
-        Laptop PC
-      </Table.Cell>
-      <Table.Cell>
-        $1999
-      </Table.Cell>
-      <Table.Cell>
-        <a
-          href="/tables"
-          className="font-medium text-blue-600 hover:underline dark:text-blue-500"
-        >
-          Edit
-        </a>
-      </Table.Cell>
-    </Table.Row>
-    <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-      <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-        Magic Mouse 2
-      </Table.Cell>
-      <Table.Cell>
-        Black
-      </Table.Cell>
-      <Table.Cell>
-        Accessories
-      </Table.Cell>
-      <Table.Cell>
-        $99
-      </Table.Cell>
-      <Table.Cell>
-        <a
-          href="/tables"
-          className="font-medium text-blue-600 hover:underline dark:text-blue-500"
-        >
-          Edit
-        </a>
-      </Table.Cell>
-    </Table.Row>
-  </Table.Body>
-</Table>
+    <div class="relative shadow-md sm:rounded-lg">
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 pt-10px">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="py-3 px-6">
+                    Meet Up Description
+                </th>
+                <th scope="col" class="py-3 px-6">
+                    Mountain
+                </th>
+                <th scope="col" class="py-3 px-6">
+                    Lift
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Apple MacBook Pro 17"
+                </th>
+                <td class="py-4 px-6">
+                    Sliver
+                </td>
+                <td class="py-4 px-6">
+                    Laptop
+                </td>
+               
+                <td class="py-4 px-6 text-right">
+                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                </td>
+            </tr>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Microsoft Surface Pro
+                </th>
+                <td class="py-4 px-6">
+                    White
+                </td>
+                <td class="py-4 px-6">
+                    Laptop PC
+                </td>
+                
+                <td class="py-4 px-6 text-right">
+                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                </td>
+            </tr>
+            <tr class="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Magic Mouse 2
+                </th>
+                <td class="py-4 px-6">
+                    Black
+                </td>
+                <td class="py-4 px-6">
+                    Accessories
+                </td>
+                
+                <td class="py-4 px-6 text-right">
+                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
   )
 }
 
