7cea28f49f562567c88f14632fcb0200b1ab9022
Kelan6 <101379206+Kelan6@users.noreply.github.com>
2022-09-23 11:40:44 -0600

    controlled form

--- client/src/EventForm.js
+++ client/src/EventForm.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {useHistory} from 'react-router-dom'
-import {Button,Label,TextInput,Checkbox, Modal} from 'flowbite-react'
+import {Button,Label,TextInput, Modal} from 'flowbite-react'
 
-function EventForm() {
+function EventForm({currentUser}) {
+
 let history = useHistory()
 
+const [formData, setFormData]=useState({
+    name: '',
+    mountain_id:'',
+    user_id: currentUser.id,
+    time: ''
+})
 
 function handleCreate(){
     console.log('post')
@@ -13,6 +20,11 @@ function handleCreate(){
 function handleGoBack(){
     history.goBack()
 }
+
+function handleChange(e){
+    setFormData({...formData,[e.target.name]: e.target.value})
+}
+console.log(formData)
   return (
     <React.Fragment>
   <Modal
@@ -34,7 +46,10 @@ function handleGoBack(){
             />
           </div>
           <TextInput
+            onChange={handleChange}
             id="description"
+            name='name'
+            value={formData.name}
             placeholder="Where we shredding and how?"
             required={true}
           />
@@ -46,11 +61,13 @@ function handleGoBack(){
               value="Which Mountain 🏔"
             />
           </div>
-          <TextInput
-            id="mountain"
-            placeholder="Keystone, Breck or Crested?"
-            required={true}
-          />
+          <label for="mountains" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Select an mountain</label>
+<select name='mountain_id' onChange={handleChange}id="mountains" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+  <option selected>Choose a Mountain</option>
+  <option value="1"> Keystone </option>
+  <option value="2"> Breckenridge </option>
+  <option value="3"> Crested Butte </option>
+</select>
         </div>
         <div>
           <div className="mb-2 block">
@@ -65,22 +82,9 @@ function handleGoBack(){
             required={true}
           />
         </div>
-        <div>
-          <div className="mb-2 block">
-            <Label
-              id="who"
-              value="Who? 🏂"
-            />
-          </div>
-          <TextInput
-            id="who"
-            placeholder='Who?'
-            required={true}
-          />
-        </div>
         
         <div className="w-full">
-          <Button onClick={handleCreate}>
+          <Button onSubmit={handleCreate}>
             Create +
           </Button>
         </div>
