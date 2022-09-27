6111ae255746cbf9d293c8c974d0ff41765839ea
Kelan6 <101379206+Kelan6@users.noreply.github.com>
2022-09-27 15:44:23 -0600

    signup working just need help styling the checkbox

--- client/src/Signup.js
+++ client/src/Signup.js
@@ -73,15 +73,7 @@ function Signup({ setCurrentUser, setLoggedIn }) {
                 <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
                 <input type="password" value={password} onChange={handleChange} name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
               </div>
-              
-              <div class="flex items-start">
-                <div class="flex items-center h-5">
-                  <input id="terms" aria-describedby="terms" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="" />
-                </div>
-                <div class="ml-3 text-sm">
-                  <label for="terms"  name='snowboarder' value={snowboarder} onChange={handleChange} class="font-light text-gray-500 dark:text-gray-300"> I shred on 1 board 🏂 </label>
-                </div>
-              </div>
+              <input name='snowboarder' value={snowboarder} onChange={handleChange} type="checkbox"/> <span class='text-white'> I prefer 1 board over 2 🏂 </span>
               <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
               <p class="text-sm font-light text-gray-500 dark:text-gray-400">
                 Already have an account? <a href='./login' class="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
