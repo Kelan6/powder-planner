6b242ce57dd5becdcf02878873e53bcf31d713da
Kelan6 <101379206+Kelan6@users.noreply.github.com>
2022-09-27 15:09:12 -0600

    login modal working, routes to sign up page

--- client/src/Login.js
+++ client/src/Login.js
@@ -51,14 +51,14 @@ function Login({ setLoggedIn, setCurrentUser }) {
             <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
               
             </h1>
-            <form class="space-y-4 md:space-y-6" action="#">
+            <form class="space-y-4 md:space-y-6" action="#" onSubmit={handleSubmit}>
               <div>
-                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""/>
+                <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Name</label>
+                <input  type='text' name="name" id="name" value={name} onChange={handleChange} class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Name" required=""/>
               </div>
               <div>
                 <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
+                <input type="password" name="password" id="password" value={password} onChange={handleChange} placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
               </div>
               <div class="flex items-center justify-between">
                 <div class="flex items-start">
@@ -71,7 +71,7 @@ function Login({ setLoggedIn, setCurrentUser }) {
                 </div>
                 <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
               </div>
-              <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+              <button type="submit" name='login' class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
               <p class="text-sm font-light text-gray-500 dark:text-gray-400">
                 Don’t have an account yet? <a href='./signup' class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
               </p>
@@ -84,34 +84,7 @@ function Login({ setLoggedIn, setCurrentUser }) {
   )
 }
 
+
+
 export default Login
 
-// {/* <div className="">
-//     //   <h1>Login</h1>
-//     //   <div className="form-container">
-//     //     <form className="login-signup-form" onSubmit={handleSubmit}>
-//     //       <section className="input-form">
-//     //         <label>Name:</label>
-//     //         <input */}
-//     //           type="text"
-//     //           name="name"
-//     //           value={name}
-//     //           onChange={handleChange}
-//     //           placeholder="  Enter Name">
-//     //         </input>
-//     //       </section>
-//     //       <section className="input-form">
-//     //         <label>Password:</label>
-//     //         <input
-//     //           type="password"
-//     //           name="password"
-//     //           value={password}
-//     //           onChange={handleChange}
-//     //           placeholder="  Enter Password" />
-//     //       </section>
-//     //       <section className="input-form">
-//     //         <input className="lg-su-button" type="submit" value="Login!" />
-//     //       </section>
-//     //     </form>
-//     //   </div>
-//     // </div>
