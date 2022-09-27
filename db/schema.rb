434a488a819b05bc64903e9f65f9adbfcda25366
Kelan6 <101379206+Kelan6@users.noreply.github.com>
2022-09-27 11:42:10 -0600

    mountains db updated for cards

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_23_035850) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_27_172736) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,9 +33,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_23_035850) do
   create_table "mountains", force: :cascade do |t|
     t.string "title"
     t.string "address"
-    t.integer "elevation"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "elevation"
+    t.text "description"
+    t.string "image"
+    t.string "map"
   end
 
   create_table "users", force: :cascade do |t|
@@ -46,4 +49,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_23_035850) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
 end
