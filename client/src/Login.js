ed707108dc9e1504a0c4e046f576d5e382840f10
Kelan6 <101379206+Kelan6@users.noreply.github.com>
2022-09-27 14:22:43 -0600

    login start

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,76 +1,117 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
+import snowflake from './assets/snowflake.png'
 
-function Login({setLoggedIn, setCurrentUser}) {
-    const [formData, setFormData] = useState({
-        name: "",
-        password: "",
-      });
-    
-      const history = useHistory()
-    
-      const { name, password } = formData;
-    
-      function handleChange(e) {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
-      }
-    
-      function handleSubmit(e) {
-        e.preventDefault();
-        fetch(`/login`, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(formData),
-        }).then((res) => {
-          if (res.ok) {
-            res.json().then((formData) => {
-              setCurrentUser(formData);
-              setLoggedIn(formData)
-              history.push("/");
-            });
-          } else {
-            res.json().then((errors) => {
-              console.error(errors)
-            });
-          }
+function Login({ setLoggedIn, setCurrentUser }) {
+  const [formData, setFormData] = useState({
+    name: "",
+    password: "",
+  });
+
+  const history = useHistory()
+
+  const { name, password } = formData;
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch(`/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    }).then((res) => {
+      if (res.ok) {
+        res.json().then((formData) => {
+          setCurrentUser(formData);
+          setLoggedIn(formData)
+          history.push("/");
+        });
+      } else {
+        res.json().then((errors) => {
+          console.error(errors)
         });
       }
+    });
+  }
   return (
-    <div className="form-box">
-    <div className="login-box">
-      <h1>Login</h1>
-      <div className="form-container">
-        <form className="login-signup-form" onSubmit={handleSubmit}>
-          <section className="input-form">
-            <label>Name:</label>
-            <input
-              type="text"
-              name="name"
-              value={name}
-              onChange={handleChange}
-              placeholder="  Enter Name">
-            </input>
-          </section>
-          <section className="input-form">
-            <label>Password:</label>
-            <input
-              type="password"
-              name="password"
-              value={password}
-              onChange={handleChange}
-              placeholder="  Enter Password" />
-          </section>
-          <section className="input-form">
-            <input className="lg-su-button" type="submit" value="Login!" />
-          </section>
-        </form>
+    <section class="bg-gray-50 dark:bg-gray-900">
+      <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+          <img class="w-8 h-8 mr-2" src={snowflake} alt="logo"/>
+            Mountain Meetup
+        </a>
+        <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              Sign in to your account
+            </h1>
+            <form class="space-y-4 md:space-y-6" action="#">
+              <div>
+                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""/>
+              </div>
+              <div>
+                <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
+              </div>
+              <div class="flex items-center justify-between">
+                <div class="flex items-start">
+                  <div class="flex items-center h-5">
+                    <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required=""/>
+                  </div>
+                  <div class="ml-3 text-sm">
+                    <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
+                  </div>
+                </div>
+                <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+              </div>
+              <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+              <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                Don’t have an account yet? <a href='./signup' class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+              </p>
+            </form>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
+    </section>
+
   )
 }
 
 export default Login
+
+// {/* <div className="">
+//     //   <h1>Login</h1>
+//     //   <div className="form-container">
+//     //     <form className="login-signup-form" onSubmit={handleSubmit}>
+//     //       <section className="input-form">
+//     //         <label>Name:</label>
+//     //         <input */}
+//     //           type="text"
+//     //           name="name"
+//     //           value={name}
+//     //           onChange={handleChange}
+//     //           placeholder="  Enter Name">
+//     //         </input>
+//     //       </section>
+//     //       <section className="input-form">
+//     //         <label>Password:</label>
+//     //         <input
+//     //           type="password"
+//     //           name="password"
+//     //           value={password}
+//     //           onChange={handleChange}
+//     //           placeholder="  Enter Password" />
+//     //       </section>
+//     //       <section className="input-form">
+//     //         <input className="lg-su-button" type="submit" value="Login!" />
+//     //       </section>
+//     //     </form>
+//     //   </div>
+//     // </div>
