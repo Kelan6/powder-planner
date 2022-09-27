be6689934265e28bc1c78b4e922179c16bb0bf01
Kelan6 <101379206+Kelan6@users.noreply.github.com>
2022-09-27 14:13:01 -0600

    about page done, with about para written

--- client/src/About.js
+++ client/src/About.js
@@ -1,37 +1,23 @@
 import React from 'react'
-import {Carousel} from 'flowbite-react'
-import k1 from './assets/k1.png'
-import pic3 from './assets/pic3.jpg'
-import k7 from './assets/k7.jpeg'
-import pic from './assets/pic.jpeg'
+import p6 from './assets/p6.png'
+
 
 function About() {
   return (
     <div>
-    <div className='pt-2 flex justify-center'>
-    <div className= 'h-96 w-2/3'>
-    <Carousel >
-    <img
-        src={pic}
-        alt="..."
-      />
-      <img 
-        src={k1}
-        alt="..."
-      />
-      <img
-        src={pic3}
-        alt="..."
-      />
-      <img
-        src={k7}
-        alt="..."
-      />
-    </Carousel>
-  </div>
-  </div>
-  <div className='pt-2 flex justify-center text-slate-50'> Inspired by ..</div>
-  </div>
+    
+   <div className='pt-10 flex justify-center'>
+    <figure class="flex justify-center h-1/3 w-1/3">
+      <img class="max-w-full h-auto rounded-xl" src={p6} alt="image description" />
+    </figure>
+    </div>
+    <div class=' pt-1 text-xs text-white flex justify-center'> Taken Apr 4,2021 on top of Peak 6, Breckenridge, CO. </div>
+    <div class=' pt-14 text-white flex justify-center'> "Mountain Meetup was inspired by my Colorado Adventures! Born and mostly raised in Texas, I never thought I would see the mountains in the centennial state, let alone snowboard their Peaks. </div>
+    <div class='text-white flex justify-center'> I quickly became addicted to the snowboarding commuity and the adrenaline associated with it. I wanted to share my new interest with my family and friends.</div>
+    <div class='text-white flex justify-center'> Mountain Meetup was a quick and easy way to create meet ups on different mountains at different times. I plan to continue to update and improve this app as my adventures continue.</div>
+    <div class='text-white flex justify-center'> Cheers to many more! Hope to see you out there!"</div>
+    <div class='text-white flex justify-center'> - Kelan</div>
+    </div>
   )
 }
 
--- client/src/MountainCard.js
+++ client/src/MountainCard.js
@@ -1,12 +1,8 @@
 import React from 'react'
-import { Card, Dropdown, Avatar } from 'flowbite-react'
+import {Card, Dropdown} from 'flowbite-react'
 
 function MountainCard({ mountain }) {
 
-    console.log(mountain)
-    function handleClick() {
-    console.log('push to trail map')
-    }
 
     return (
         <Card className='pt-10px' href="#">
@@ -28,37 +24,37 @@ function MountainCard({ mountain }) {
                 </p>
                 <div> <a className='text-white hover:underline'  target='_blank' href={mountain.map}> Trail Map </a> </div>
                 <Dropdown label="Lifts">
-                    <Dropdown.Item onClick={handleClick}>
+                    <Dropdown.Item >
                         {mountain.lifts[0].title}
                     </Dropdown.Item>
                     <Dropdown.Item >
                         {mountain.lifts[1].title}
                     </Dropdown.Item>
-                    <Dropdown.Item onClick={handleClick}>
+                    <Dropdown.Item >
                         {mountain.lifts[2].title}
                     </Dropdown.Item>
-                    <Dropdown.Item onClick={handleClick}>
+                    <Dropdown.Item >
                         {mountain.lifts[3].title}
                     </Dropdown.Item>
-                    <Dropdown.Item onClick={handleClick}>
+                    <Dropdown.Item >
                         {mountain.lifts[4].title}
                     </Dropdown.Item>
-                    <Dropdown.Item onClick={handleClick}>
+                    <Dropdown.Item >
                         {mountain.lifts[5].title}
                     </Dropdown.Item>
-                    <Dropdown.Item onClick={handleClick}>
+                    <Dropdown.Item >
                         {mountain.lifts[6].title}
                     </Dropdown.Item>
-                    <Dropdown.Item onClick={handleClick}>
+                    <Dropdown.Item >
                         {mountain.lifts[7].title}
                     </Dropdown.Item>
-                    <Dropdown.Item onClick={handleClick}>
+                    <Dropdown.Item >
                         {mountain.lifts[8].title}
                     </Dropdown.Item>
-                    <Dropdown.Item onClick={handleClick}>
+                    <Dropdown.Item >
                         {mountain.lifts[9].title}
                     </Dropdown.Item>
-                    <Dropdown.Item onClick={handleClick}>
+                    <Dropdown.Item >
                         {mountain.lifts[10].title}
                     </Dropdown.Item>
                 </Dropdown>
