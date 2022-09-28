ff14ddb42b08f7c58c003ced60abdfe81a2d5e81
Kelan6 <101379206+Kelan6@users.noreply.github.com>
2022-09-27 20:41:38 -0600

    home page done with typewrite and images, need to stack

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,29 +1,73 @@
 import React from 'react'
 import { TypeAnimation } from 'react-type-animation';
+import pic2 from './assets/pic2.jpg'
+import sb1 from './assets/sb1.webp'
+import gsd3 from './assets/gsd3.jpeg'
+import smb from './assets/smb.jpeg'
+import m1 from './assets/m1.jpeg'
+import sk1 from './assets/sk1.jpeg'
 
 function Home() {
-  
+
   return (
     <div className='flex justify-center'>
-    <div className='text-white mt-10'>
-      <h1 className='font-serif text-8xl'>Welcome to Mountain Meetup</h1>
-      <div className='font-serif mt-10 flex justify-center'>
-    <TypeAnimation 
-    sequence={[
-    'An App Made for Snowboarders 🏂',
-    1000,
-    'An App Made for Skiers ⛷',
-    1000,
-    'An App Made for All Snow Fanatics ❄️',
-    1000,
-    ]}
-    speed={50}
-    style={{ fontSize: '2em' }}
-    wrapper="span" // Animation will be rendered as a <span>
-    repeat={Infinity} // Repeat this Animation Sequence infinitely
-    />
-    </div>
-    </div>
+      <div className='text-white mt-10'>
+        <h1 className='font-serif text-8xl'>Welcome to Mountain Meetup</h1>
+        <div className='font-serif mt-10 flex justify-center'>
+          <TypeAnimation
+            sequence={[
+              'An App Made for Snowboarders 🏂',
+              1000,
+              'An App Made for Skiers ⛷',
+              1000,
+              'An App Made for Adventurers 🏔',
+              1000,
+              'An App Made for All Snow Fanatics ❄️',
+              1000,
+            ]}
+            speed={50}
+            style={{ fontSize: '2em' }}
+            wrapper="span" 
+            repeat={Infinity} 
+          />
+        </div>
+      </div>
+      <div className=''>
+        <section class="overflow-hidden text-gray-700">
+          <div class="container px-5 py-2 mx-auto lg:pt-24 lg:px-32">
+            <div class="flex flex-wrap -m-1 md:-m-2">
+              <div class="flex flex-wrap w-1/2">
+                <div class="w-1/2 p-1 md:p-2">
+                  <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+                    src={pic2} />
+                </div>
+                <div class="w-1/2 p-1 md:p-2">
+                  <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+                    src={gsd3} />
+                </div>
+                <div class="w-full p-1 md:p-2">
+                  <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+                    src={sb1} />
+                </div>
+              </div>
+              <div class="flex flex-wrap w-1/2">
+                <div class="w-full p-1 md:p-2">
+                  <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+                    src={sk1} />
+                </div>
+                <div class="w-1/2 p-1 md:p-2">
+                  <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+                    src={smb} />
+                </div>
+                <div class="w-1/2 p-1 md:p-2">
+                  <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
+                    src={m1} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   )
 }
