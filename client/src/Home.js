6b5f77297cdf4d4567e43b612217baf47539f1c6
Kelan6 <101379206+Kelan6@users.noreply.github.com>
2022-09-27 19:56:33 -0600

    typewrite home page

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import { TypeAnimation } from 'react-type-animation';
 
 function Home() {
   
   return (
-    <div>
-      <h1> POWDER PLANNER </h1>
+    <div className='flex justify-center'>
+    <div className='text-white mt-10'>
+      <h1 className='font-serif text-8xl'>Welcome to Mountain Meetup</h1>
+      <div className='font-serif mt-10 flex justify-center'>
+    <TypeAnimation 
+    sequence={[
+    'An App Made for Snowboarders 🏂',
+    1000,
+    'An App Made for Skiers ⛷',
+    1000,
+    'An App Made for All Snow Fanatics ❄️',
+    1000,
+    ]}
+    speed={50}
+    style={{ fontSize: '2em' }}
+    wrapper="span" // Animation will be rendered as a <span>
+    repeat={Infinity} // Repeat this Animation Sequence infinitely
+    />
+    </div>
+    </div>
     </div>
   )
 }
