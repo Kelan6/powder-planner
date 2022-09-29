2ba087605ecb75bb04b275ecba305089989b6f28
Kelan6 <101379206+Kelan6@users.noreply.github.com>
2022-09-29 16:30:54 -0600

    update

--- client/src/MountainCard.js
+++ client/src/MountainCard.js
@@ -5,11 +5,13 @@ function MountainCard({ mountain }) {
 
 
     return (
-       
-        <Card className='flex flex-row pt-10'>
+       <div>
+        <Card className='flex flex-row pt-10'
+        >
             <div>
             <img class="max-w-lg h-auto" src={mountain.image} alt=''/>
             </div>
+            <div className='flex flex-row'>
             <div className=' w-6/12'>
                 <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                     {mountain.title}
@@ -25,7 +27,7 @@ function MountainCard({ mountain }) {
                 </p>
                 <p className="font-normal text-gray-700 dark:text-gray-400">
                 </p>
-                <div> <a className='text-white hover:underline'  target='_blank' href={mountain.map}> Trail Map </a> </div>
+                <div> <a className='pt-1 pb-2 text-gray-900 dark:text-white hover:underline'  target='_blank' href={mountain.map}> Trail Map </a> </div>
                 <Dropdown className='mt-1' label="Lifts">
                     <Dropdown.Item >
                         {mountain.lifts[0].title}
@@ -62,7 +64,9 @@ function MountainCard({ mountain }) {
                     </Dropdown.Item>
                 </Dropdown>
                 </div>
+                </div>
         </Card>
+        </div>
     )
 }
 
