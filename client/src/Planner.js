bea2085806cd579b982adac2e153bfe58eb10ace
Kelan6 <101379206+Kelan6@users.noreply.github.com>
2022-09-30 13:42:53 -0600

    planner cancel column fixed

--- client/src/Planner.js
+++ client/src/Planner.js
@@ -74,13 +74,15 @@ function Planner({ currentUser, setCurrentUser }) {
             <th scope="col" class="py-3 px-6">
               Date / Time
             </th>
+            <th scope="col" class="py-3 px-6">
+              
+            </th>
           </tr>
         </thead>
         <tbody>
           {eventsArr}
         </tbody>
       </table>
-      {/* <button class='text-white' > Create Meet Up + </button> */}
       <button onClick={handleAddClick} type="button" class="py-2.5 px-5 mr-2 text-sm font-medium text-white-600 bg-white rounded-xl border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 inline-flex items-center">
     <svg aria-hidden="true" role="status" class="inline mr-2 w-4 h-4 text-gray-200 animate-spin dark:text-gray-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
