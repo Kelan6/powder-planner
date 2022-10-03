cc07bda5c4a64719d7b99004ffe8445fc8c186ad
Kelan6 <101379206+Kelan6@users.noreply.github.com>
2022-10-03 11:57:53 -0600

    footer fixed

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -2,13 +2,14 @@ import React from 'react'
 
 function Footer() {
   return (
-    
-<footer class="pt-10 bg-white rounded-lg mt-8 md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
-    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="/" class="hover:underline">Mountain Meetup ™</a>. All Rights Reserved.
+    <div className='justify-between fixed bottom-0 left-0'>
+<footer class=" mt-auto bg-inherit rounded-lg  md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="/" class="hover:underline">Mountain Meetup™</a>
     </span>
+    
     <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
-            <a href="https://www.linkedin.com/in/kelanhamman/" target='_blank'class="mr-4 hover:underline md:mr-6 ">LinkedIn</a>
+            <a href="https://www.linkedin.com/in/kelanhamman/" target='_blank'class="mr-4 hover:underline md:mr-6 "> ․ LinkedIn</a>
         </li>
         <li>
             <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
@@ -21,6 +22,7 @@ function Footer() {
         </li>
     </ul>
 </footer>
+</div>
 
   )
 }
