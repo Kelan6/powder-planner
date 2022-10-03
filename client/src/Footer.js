ec20806366850813bd07389df9c6732262c72397
Kelan6 <101379206+Kelan6@users.noreply.github.com>
2022-10-03 10:44:36 -0600

    testing

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     
 <footer class="p-4 bg-white rounded-lg mt-8 md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
-    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com/" class="hover:underline">Mountain Meetup ™</a>. All Rights Reserved.
+    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="/" class="hover:underline">Mountain Meetup ™</a>. All Rights Reserved.
     </span>
     <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
